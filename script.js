c45fb45a3facbc60e42cf30a1fc270e9b397fb2b
Mpho Mafata <131434881+mpho-mafata@users.noreply.github.com>
2024-10-18 07:26:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,14 @@ function closeNavbar() {
     column-gap: 20px;
 }
 
+let mysBtn = document
+    .getElementById('scrollbuttonid');
+
+window.addEventListener('scroll', function () {
+    if (document.body.scrollTop > 20
+        || document.documentElement.scrollTop > 20) {
+        mysBtn.style.display = 'block';
+    } else {
+        mysBtn.style.display = 'none';
+    }
+});
