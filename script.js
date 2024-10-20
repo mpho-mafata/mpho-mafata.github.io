2c26ab3263b732e4932a9c7c5e2433d497c39804
Mpho Mafata <131434881+mpho-mafata@users.noreply.github.com>
2024-10-20 06:58:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,14 +28,22 @@ function closeNavbar() {
     column-gap: 20px;
 }
 
-let mysBtn = document
-    .getElementById('scrollbuttonid');
+// Get the scroll button:
+let mybutton = document.getElementById("scrollbutton");
 
-window.addEventListener('scroll', function () {
-    if (document.body.scrollTop > 20
-        || document.documentElement.scrollTop > 20) {
-        mysBtn.style.display = 'block';
-    } else {
-        mysBtn.style.display = 'none';
-    }
-});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
