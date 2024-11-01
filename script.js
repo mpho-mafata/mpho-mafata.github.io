b79bf4785462f610a6d7ac16cee25700f1d1ebe6
Mpho Mafata <131434881+mpho-mafata@users.noreply.github.com>
2024-11-01 11:59:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -48,32 +48,3 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-
-// Selecting the html class and  
-// convert it to an object
-
-const sharebtn = 
-    document.querySelector('.sharebtn')
-
-// Creating a bool variable for changing
-// the image of share button 
-let bool = 0
-
-// Adding an event listener
-sharebtn.addEventListener('click', () => {
-
-    // As we clicked the mouse over
-    // the share button the bool value.
-    //  get flipped and then working of
-    // if-else loop get starts
-    bool = !bool
-    
-    if (bool == 0) {
-        sharebtn.innerHTML =
-            '<i class="far fa-share-square"></i>'
-    } else {
-        sharebtn.innerHTML =
-            '<i class="fas fa-times"></i>'
-    }
-})
-
