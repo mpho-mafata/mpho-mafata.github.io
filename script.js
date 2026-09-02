37f940a913220d970d59106bddd22d899b04d36d
Mpho Mafata <131434881+mpho-mafata@users.noreply.github.com>
2026-09-02 10:29:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -57,6 +57,6 @@ document.getElementById('twitter-share').addEventListener('click', function() {
 document.getElementById('linkedin-share').addEventListener('click', function() {
     const url = encodeURIComponent(window.location.href);
     const text = encodeURIComponent(document.title);
-    const shareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${url}&text="Check out this website"`;
+    const shareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${url}`;
     window.open(shareUrl, '_blank');
 });
